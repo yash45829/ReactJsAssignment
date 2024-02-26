1793b992ace9a4d4284459d521410820bdd731f2
yash <yash45829@gmail.com>
2024-02-26 13:47:37 +0530

    Footer: footer part completed with responsiveness

--- src/Components/SignUpForm.jsx
+++ src/Components/SignUpForm.jsx
@@ -1,7 +1,7 @@
 function SignUpForm (){
 
     return (
-     <div className="py-2 my-4 flex flex-col gap-2 sm:flex-row sm:justify-between sm:items-center">
+     <div className="py-2 my-8 flex flex-col gap-2 sm:flex-row sm:justify-between sm:items-center">
         <h1 className="text-2xl text-gray-800 font-semibold">Sign up and get exclusive special deals</h1>
         <form className="">
             <input type="text" className="rounded-s-lg py-2 px-2 focus:outline-none focus:shadow"/>
--- src/Sections/Footer.jsx
+++ src/Sections/Footer.jsx
@@ -1,7 +1,37 @@
 function Footer (){
 
     return (
-     <>fooeter</>
+     <main className=" flex flex-col pt-8 pb-24 sm:flex-row justify-start w-full mb-0   sm:justify-between sm:items-start bg-slate-950 text-gray-400">
+        <div className="flex justify-evenly sm:w-[50%] ">
+        <div>
+        <h2 className="font-semibold">CATEGORIES</h2>
+        <ul>
+            <li className=" text-sm my-2">Web Builder</li>
+            <li className=" text-sm my-2">Hosting</li>
+            <li className=" text-sm my-2">Data center</li>
+            <li className=" text-sm my-2">Robotic automation</li>
+        </ul>
+        </div>
+        <div>
+        <h2 className="font-semibold">CONTACT</h2>
+        <ul>
+            <li className=" text-sm my-2">Contact</li>
+            <li className=" text-sm my-2">Privacy policy</li>
+            <li className=" text-sm my-2">Terms of service</li>
+            <li className=" text-sm my-2">categories</li>
+            <li className=" text-sm my-2">About</li>
+        </ul>
+        </div>
+        </div>
+        <div className=" sm:w-[50%] pl-4 " >
+            
+        <h2 className="font-semibold ">Location</h2>
+        <ul>
+            <li className=" text-sm my-2 ">United States</li>
+        </ul>
+      
+        </div>
+     </main>
     )
 }
 
